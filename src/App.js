6f1b6bcf5b58192659609faa3433b5e95932bed8
Achraf <achraf.tarifa@enis.tn>
2021-02-23 12:45:33 +0100

    add-input-text
    Signed-off-by: Achraf <achraf.tarifa@enis.tn>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import React, { useState } from 'react' ;
 import './App.css';
+import From from './components/Form';
+import Todolist from './components/Todolist'
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  
   return (
     <div className="App">
       <header>
         <h1>My to do list</h1>
       </header>
+      <From setInputText={setInputText}/>
+      <Todolist />
     </div>
   );
-}
+};
 
 export default App;
